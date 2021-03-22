f665dd76e511b787207e50bb66d4208441021bac
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-22 17:50:40 +0100

    milestone 1 completa

--- Milestone icons/dati.js
+++ Milestone icons/dati.js
@@ -1,115 +1,118 @@
 $(document).ready(function(){
 
+	var icons = [
+		{
+			name: 'cat',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'crow',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'dog',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'dove',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'dragon',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'horse',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'hippo',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'fish',
+			prefix: 'fa-',
+			type: 'animal',
+			family: 'fas'
+		},
+		{
+			name: 'carrot',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'apple-alt',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'lemon',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'pepper-hot',
+			prefix: 'fa-',
+			type: 'vegetable',
+			family: 'fas'
+		},
+		{
+			name: 'user-astronaut',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		},
+		{
+			name: 'user-graduate',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		},
+		{
+			name: 'user-ninja',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		},
+		{
+			name: 'user-secret',
+			prefix: 'fa-',
+			type: 'user',
+			family: 'fas'
+		}
+	];
 
-	var row = document.getElementById("row");
-	row.appendChild(card);
-
-document.getElementById()
-
-
-
-
-
+	for (var i = 0; i < icons.length; i++) {
+		$('.row').append(`<div class="col-3">
+				 <div class="card">
+					<div class="container-elements">
+					<div class="i-container">
+					<i class="${icons[i].family} ${icons[i].prefix}${icons[i].name}"></i>
+				 </div>
+				 <label class="label-icons">${icons[i].name}</label>
+				 </div>
+				 </div>
+			   </div>`);
+	}
 });
 
-var icons = [
-	{
-		name: 'cat',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'crow',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dog',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dove',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'dragon',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'horse',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'hippo',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'fish',
-		prefix: 'fa-',
-		type: 'animal',
-		family: 'fas'
-	},
-	{
-		name: 'carrot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'apple-alt',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'lemon',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'pepper-hot',
-		prefix: 'fa-',
-		type: 'vegetable',
-		family: 'fas'
-	},
-	{
-		name: 'user-astronaut',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-graduate',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-ninja',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	},
-	{
-		name: 'user-secret',
-		prefix: 'fa-',
-		type: 'user',
-		family: 'fas'
-	}
-];
 
 
 function functionName() {
