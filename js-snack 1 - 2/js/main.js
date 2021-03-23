114a08cf1d6078d2b9bf686b1e78613673cc8b26
sonia-cerasaro <sonia.cerasaro@gmail.com>
2021-03-23 17:50:15 +0100

    snacks ok

--- js-snack 1 - 2/js/main.js
+++ js-snack 1 - 2/js/main.js
@@ -18,50 +18,45 @@ var bici = [
     peso: 2
   },
 ];
-
-// creo una variabile dove inserisci la bici punto di partenza
-let min = bici[0].peso;
-
-// qui cicla ogni elemento nella variabile bici e dice che se
-// il peso dell'elemento e minore della bici minore verra'
-// salvato nella var con etichetta min.
-bici.forEach((element) => {
-
-  if (element.peso < min) {
-    min = element.peso;
-  }
-});
-
-var biciPiccola = bici.filter((element) => element.peso == min);
-console.log(`La bici piu piccola e ${biciPiccola[0].nome} e il peso e' di ${biciPiccola[0].peso}`);
-
-
-// filtra gli elementi nella bici e quando trovi un peso uguale
- // a min assegna il valore a bici Piccola
+//
+// // creo una variabile dove inserisci la bici punto di partenza
+// let min = bici[0].peso;
+//
+// // qui cicla ogni elemento nella variabile bici e dice che se
+// // il peso dell'elemento e minore della bici minore verra'
+// // salvato nella var con etichetta min.
+// bici.forEach((element) => {
+//
+//   if (element.peso < min) {
+//     min = element.peso;
+//   }
+// });
+//
 // var biciPiccola = bici.filter((element) => element.peso == min);
 // console.log(`La bici piu piccola e ${biciPiccola[0].nome} e il peso e' di ${biciPiccola[0].peso}`);
-
-// JSNACK 2
-// Si scriva una funzione che accetti tre argomenti,
-// un array e due numeri (a più piccolo di b).
-// La funzione ritornerà un nuovo array con i valori che hanno
-// la posizione compresa tra i due numeri
-// ES. [1,2,3,4,5,6,7,8] => [3,4,5,6] (a=2, b=5)
-
-
-const myArray = [1, 2, 3, 4, 5, 6, 7];
-var inizio = parseInt(prompt('dammi il valore minore'));
-var fine = parseInt(prompt('dammi il valore maggiore'));
-
-var arrayEstratto = estrazioneValori(myArray, inizio, fine);
-console.log(arrayEstratto);
-
-function estrazioneValori(array, inizio, fine) {
-  return array.slice(inizio, fine);
-}
 //
-// // SECONDA OPZIONE
 //
+// // JSNACK 2
+// // Si scriva una funzione che accetti tre argomenti,
+// // un array e due numeri (a più piccolo di b).
+// // La funzione ritornerà un nuovo array con i valori che hanno
+// // la posizione compresa tra i due numeri
+// // ES. [1,2,3,4,5,6,7,8] => [3,4,5,6] (a=2, b=5)
+//
+//
+// const myArray = [1, 2, 3, 4, 5, 6, 7];
+// var inizio = parseInt(prompt('dammi il valore minore'));
+// var fine = parseInt(prompt('dammi il valore maggiore'));
+//
+// var arrayEstratto = estrazioneValori(myArray, inizio, fine);
+// console.log(arrayEstratto);
+//
+// function estrazioneValori(array, inizio, fine) {
+//   return array.slice(inizio, fine);
+// }
+
+//SECONDA OPZIONE
+
 const myArray = [1, 2, 3, 4, 5, 6, 7];
 var inizio = 3;
 var fine = inizio + 2;
